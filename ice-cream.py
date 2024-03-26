6886300eb91533d916cf0651acbe3d01e8aa071e
Madeline Mariano <mam5090@rit.edu>
2024-03-26 19:25:37 -0400

    formatted most of GUI, added TODOs for rest of tasks

--- ice-cream.py
+++ ice-cream.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 
 # Create an empty list for the transactions of all accounts
-transactions = list()
+# transactions = list()
 
 # Create variables that indicate the inventory of each item
 VANILLA = 0
@@ -11,78 +11,110 @@ SPRINKLES = 0
 WHIP_CREAM = 0
 HOT_F = 0
 
-# def place_order():
-#     # Read in amount
-#     scoops = float(ent_scoops.get())
-#     flavor =
-#     sp =
-#     whip =
-#     hotf =
-#     fn_update_outputs()
+# Create variables for financial data
+SALES = 0.00
+EXPENSES = 0.00
+PROFIT = 0.00
+
+# variable with user feedback
+FEEDBACK = " "
 
 
-def cmd_remove():
-    fn_update_outputs()
+def add_inventory():
+    pass
+# TODO
+#   get values from checked boxes of what to update
+#   update global variables with integer values
+#   see write up for amounts
+#   update expenses with the cost
+#   update profit by subtracting expenses from sales
 
 
-def fn_update_outputs(event=None):
-    #pass
-    balance = 0
-    lst_transactions.delete(0, tk.END)
-    # Loop through all the transactions and filter out those that match the combobox
-    for transaction in transactions:
-        # Test to see if the current transaction matches the combobox
-        if transaction["account"] == cbo_accounts.get():
-            # Add the value to the balance
-            balance = balance + transaction["amount"]
-            # Add the value to the listbox
-            lst_transactions.insert(tk.END, transaction["amount"])
+def place_order():
+    pass
+# TODO
+#   get flavor from radio buttons
+#   get values from checked boxes for toppings
+#   check inventory that the items are in stock; display error message if not
+#   place order and call financial data function
+#   The first scoop is $3.00. Each scoop extra is $1.00; calculate cost
+#   update the inventory
+#   update financial data
 
-    # Output the balance
-    lbl_balance["text"] = balance
+    # scoops = int(ent_scoops.get())
+    # cost = 0.00
+    # ADD AN IF GUARD HERE FOR AMT OF SCOOPS TO UPDATE COST
 
+
+def update_finances():
+    pass
+# TODO
+#   Each time the user adds to inventory, expenses will be increased.
+#   Each time an order is made, sales will be increased.
+#   the profit is calculated by subtracting the expenses from the sales.
+
+
+def user_feedback():
+    pass
+# TODO
+#   update textbox with necessary error
 
 
 # Creating window
 root_window = tk.Tk()
 root_window.title("Ice Cream Shop")
-root_window.geometry("600x300")
+root_window.geometry("800x400")
 
-# Setting up input controls
-tk.Label(root_window, text="INVENTORY:").grid(row=0, column=0, sticky=tk.W)
+# INVENTORY (done)
+tk.Label(root_window, text="INVENTORY").grid(row=0, column=0, sticky=tk.W)
 tk.Label(root_window, text="Vanilla:").grid(row=1, column=0, sticky=tk.W)
 tk.Label(root_window, text="Chocolate:").grid(row=2, column=0, sticky=tk.W)
 tk.Label(root_window, text="Sprinkles:").grid(row=3, column=0, sticky=tk.W)
 tk.Label(root_window, text="Whipped cream:").grid(row=4, column=0, sticky=tk.W)
 tk.Label(root_window, text="Hot fudge:").grid(row=5, column=0, sticky=tk.W)
 
-tk.Label(root_window, text=VANILLA).grid(row=1, column=5, sticky=tk.W)
-tk.Label(root_window, text=CHOCOLATE).grid(row=2, column=5, sticky=tk.W)
-tk.Label(root_window, text=SPRINKLES).grid(row=3, column=5, sticky=tk.W)
-tk.Label(root_window, text=WHIP_CREAM).grid(row=4, column=5, sticky=tk.W)
-tk.Label(root_window, text=HOT_F).grid(row=5, column=5, sticky=tk.W)
+tk.Label(root_window, text=VANILLA).grid(row=1, column=1, sticky=tk.W)
+tk.Label(root_window, text=CHOCOLATE).grid(row=2, column=1, sticky=tk.W)
+tk.Label(root_window, text=SPRINKLES).grid(row=3, column=1, sticky=tk.W)
+tk.Label(root_window, text=WHIP_CREAM).grid(row=4, column=1, sticky=tk.W)
+tk.Label(root_window, text=HOT_F).grid(row=5, column=1, sticky=tk.W)
 
-#ADD TO INVENTORY
-
-
-
-#ORDER FORM
-# ent_scoops = tk.Entry(root_window, width=5)
-# ent_scoops.grid(row=0, column=50, sticky=tk.W)
-# tk.Button(root_window, text="Place Order", command=place_order).grid(row=2, column=2, sticky=tk.W)
-# tk.Label(root_window, text="Account:").grid(row=1, column=0, sticky=tk.W)
-#
+# ADD TO INVENTORY
+tk.Label(root_window, text="\tADD TO INVENTORY").grid(row=0, column=3)
+# TODO check boxes here for all the options (see the write up for particulars)
+# TODO button to "Add To Inventory"
+# TODO code so that pushing the button calls the proper functions
 
 
-# FINANCIAL DATA
+# ORDER FORM
+tk.Label(root_window, text="\tORDER FORM").grid(row=0, column=4)
+tk.Label(root_window, text="\tScoops:").grid(row=1, column=4)
+ent_scoops = tk.Entry(root_window, width=5)
+ent_scoops.grid(row=1, column=5, sticky=tk.W)
+# TODO add radio buttons for Vanilla/Chocolate row 2
+# TODO add check boxes for Sprinkles, row 3
+# TODO Whipped Cream, row4
+# TODO and Hot Fudge row5
+tk.Button(root_window, text="Place Order", command=place_order).grid(row=6, column=5, sticky=tk.W)
 
 
+# FINANCIAL DATA (done)
+tk.Label(root_window, text="\tFINANCIAL DATA").grid(row=0, column=6)
+tk.Label(root_window, text="\tSales:").grid(row=1, column=6, sticky=tk.W)
+tk.Label(root_window, text="\tExpenses:").grid(row=2, column=6, sticky=tk.W)
+tk.Label(root_window, text="\tProfit:").grid(row=3, column=6, sticky=tk.W)
+
+tk.Label(root_window, text=f"\t${SALES:.2f}").grid(row=1, column=7, sticky=tk.W)
+tk.Label(root_window, text=f"\t${EXPENSES:.2f}").grid(row=2, column=7, sticky=tk.W)
+tk.Label(root_window, text=f"\t${PROFIT:.2f}").grid(row=3, column=7, sticky=tk.W)
 
 
+# USER FEEDBACK (done)
+tk.Label(root_window, text="USER FEEDBACK:").grid(row=10, column=0)
+tk.Label(root_window, text=FEEDBACK).grid(row=11, column=0)
 
-# USER FEEDBACK
 
-
+# code below is copy pasted from class
 
 # transaction_type = tk.IntVar()
 # rdo_deposit = tk.Radiobutton(root_window, text="Deposit", variable=transaction_type, value=1)
